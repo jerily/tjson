8543b8fcd6bcbddd7fb04b2a0ee6bbd7c56c14a7
Neofytos Dimitriou <neo@jerily.cy>
2023-09-03 15:58:25 -0400

    basic types for triple notation (string, int, int32, int64, boolean) plus document and array - added tests

--- examples/example5-custom.tcl
+++ examples/example5-custom.tcl
@@ -7,4 +7,3 @@ puts doc_example,json=[::tjson::typed_to_json $doc_example]
 set arr_example [::tjson::custom_to_typed [list myarr array [list 0 int32 1 1 int32 2 2 string "hello\nworld" 3 boolean true]]]
 puts arr_example,typed=$arr_example
 puts arr_example,json=[::tjson::typed_to_json $arr_example]
-
