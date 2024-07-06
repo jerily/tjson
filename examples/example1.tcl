6c439b5c5f3af73e96ff7c87c1df7462edf5b218
Neofytos Dimitriou <neo@jerily.cy>
2024-07-06 04:03:36 -0400

    added test for fixed issue

--- examples/example1.tcl
+++ examples/example1.tcl
@@ -9,6 +9,6 @@ puts json_to_simple=[::tjson::json_to_simple {{"a":1,"b":2,"c":"hello"}}]
 puts list=[::tjson::json_to_typed {[1,2,3]}]
 puts json_to_typed=[::tjson::json_to_typed {{"a": 1, "b": true, "c": [1, 2, 3], "d": {"d1":"a", "d2":"b"}}}]
 puts json_to_simple=[::tjson::json_to_simple {{"a": 1, "b": true, "c": [1, 2, 3], "d": {"d1":"a", "d2":"b"}}}]
-puts typed_to_json=[::tjson::typed_to_json {M {a {N 1} b {BOOL 1} c {L {{N 1} {N 2} {N 3}}} d {M {d1 {S a} d2 {S b}}}}}]
+puts typed_to_json=[::tjson::typed_to_json {M {a {N 1} b {BOOL 1} c {L {{N 1} {N 2} {N 3}}} d {M {d1 {S a} d2 {S b}}} e {BOOL 0} f {N 123.45}}}]
 puts escape_json_string=[::tjson::escape_json_string "hello\"world\n"]
 
